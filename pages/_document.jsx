6982622d05afcabe64bca9c2529ace76f2cd04c8
KGH1113 <gangguhyeon1113@gmail.com>
2023-12-21 14:01:17 +0900

    google search console meta tag added

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,12 +7,8 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/logo192.png" />
           <meta
-            name="naver-site-verification"
-            content="b816fc58b692e18ad86238f0b555c8e6c3a142d5"
-          />
-          <meta
             name="google-site-verification"
-            content="jYqO_4s8MpFSeSPYlk0DrRYZPyIb_MKP4vaI8ujnB8k"
+            content="8G2KydASuYKSemKztK4MhB2wmyWQ-FWi-7VXrcrClWA"
           />
           <meta name="description" content="서운중학교 방송부 웹사이트" />
           <link
